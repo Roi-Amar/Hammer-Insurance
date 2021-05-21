1de3409efd46d5b1ef89478615a37c671a41dbb4
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2021-05-21 14:53:54 +0300

    .

--- src/logerSingleton/Logger.java
+++ src/logerSingleton/Logger.java
@@ -1,5 +1,10 @@
 package logerSingleton;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Logger {
 	    private static Logger instance = null;
 	    public String example;
@@ -7,10 +12,22 @@ public class Logger {
 	    private Logger() {
 	        example = "I am a singelton logger";
 	    }
-	  
+
 	    public static Logger getInstance() {
 	        if (instance == null)
 	            instance = new Logger();
 	        return instance;
+	    }
+    
+	    public ArrayList<String[]> readCSV(String pathToCsv) throws IOException {
+	    String row = null;
+	    ArrayList<String[]> readData = new ArrayList<String[]>();
+	    BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
+		    while ((row = csvReader.readLine()) != null) {
+		        String[] data = row.split(",");
+		        readData.add(data);
+		    }
+	    csvReader.close();
+	    return readData;
 	    }
 }
