20f7bd3047ffbf42193dee81f4d6e2b21973f33f
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2021-05-28 16:55:40 +0300

    added initialize to main screen for json loading. json reader works!

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -1,16 +1,19 @@
 package application;
 
 import java.io.IOException;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import logerSingleton.Logger;
 
-public class MainWindowController {
+public class MainWindowController implements Initializable{
 
     @FXML
     private AnchorPane leftPane;
@@ -55,5 +58,21 @@ public class MainWindowController {
 		}
 
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		String jasonFileName = "\\input.json";
+    	Logger logger = Logger.getInstance();
+    	String[] jsonDetails = null;
+    	try {
+			jsonDetails = logger.readJSON(jasonFileName);
+		} catch (Exception e) {
+			//popUp("There was an error reading json file:" + jasonFileName + "\n" + e.toString());
+			System.out.println("There was an error reading json file:" + jasonFileName + "\n" + e.toString());
+			e.printStackTrace();
+		}
+    	loadJasonHere.setText(jsonDetails[0]+"\n"+jsonDetails[1]+"\n"+jsonDetails[2]);;
+	}
 
 }
